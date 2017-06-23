2d106ae21398abecd28dc723a0d1d899e7857ac8
Amir Keivan Mohtashami <akmohtashami97@gmail.com>
2017-06-23 17:18:37 +0430

    Add TaskType Batch2017
    This task type requires the user to upload grader when using testing

---
+++ cms/grading/tasktypes/Batch2017.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# Contest Management System - http://cms-dev.github.io/
+# Copyright © 2017 Amir Keivan Mohtashami <akmohtashami97@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import logging
+
+from .Batch import Batch
+
+logger = logging.getLogger(__name__)
+
+
+class Batch2017(Batch):
+    def get_user_managers(self, unused_submission_format):
+        """See TaskType.get_user_managers."""
+        return ["grader.%l"]
